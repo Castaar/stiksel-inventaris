c5b3f4bb7168c11ead758d97992e86b913149a4d
NeZotVanCastaar <ab.meganck@gmail.com>
2024-03-06 16:05:12 +0100

    init project

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+
+module.exports = withPWA({
+  // next.js config
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+});
