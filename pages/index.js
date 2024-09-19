a5f98b65ba7dd3fa7a95967074fe2808c9b78296
CastaarCoder <marc@castaar.com>
2024-09-19 16:49:47 +0200

    Added total price value on homepage

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import clientPromise from "../lib/mongodb";
 
 import InfoBlock from "../components/blocks/info-block";
 
+import Link from "next/link";
+
 export default function home(props) {
   return (
     <>
@@ -14,11 +16,33 @@ export default function home(props) {
         <link rel="icon" href="/images/favicon.svg" />
       </Head>
       <main className="main main-overview">
+        <div className="d-flex justify-content-space-between flex-wrap w-100 btn-menu">
+          <Link href="/stock">
+            Stock
+          </Link>
+          <Link href="/borden">
+            Borden
+          </Link>
+          <Link href="/stock-aanvullen">
+            Stock aanvullen
+          </Link>
+          <Link href="/borden-aanvullen">
+            Borden aanvullen
+          </Link>
+        </div>
         <InfoBlock
-          value={`€ ${props.totalPrice}`}
+          value={`€ ${props.stock.price.total}`}
           title={"Waarde totale stock"}
         />
-        <InfoBlock value={"XX M2"} title={"Borden"} />
+        <div className="mt-40">
+          <InfoBlock
+            value={`€ ${props.borden.price.total}`}
+            title={"Waarde totale borden"}
+          />
+        </div>
+        {/* <div className="mt-40">
+          <InfoBlock value={"XX M2"} title={"Borden"} />
+        </div> */}
       </main>
     </>
   );
@@ -26,43 +50,59 @@ export default function home(props) {
 
 export async function getServerSideProps({ query }) {
   try {
-    const client = await clientPromise;
-    const db = client.db("stock");
+    
+    const totalPriceStock = await calculateTotalPriceForDB('stock');
+    const totalPriceBorden = await calculateTotalPriceForDB('borden');
 
-    const collections = await db.listCollections().toArray();
-
-    let totalPrice = 0;
-
-    for (const collection of collections) {
-      const collectionName = collection.name;
-      let toSkip = 0;
-      let toContinue = true;
-
-      while (toContinue) {
-        const results = await db
-          .collection(collectionName)
-          .find({})
-          .skip(toSkip)
-          .limit(20)
-          .toArray();
-
-        for (const product of results) {
-          totalPrice += product.price; // Assumes each product has a 'price' field
-        }
-
-        toSkip += 20;
-        if (results.length < 20) {
-          toContinue = false;
-        }
-      }
-    }
+    console.log(totalPriceStock)
+    console.log(totalPriceBorden)
 
     return {
       props: {
-        totalPrice: totalPrice,
+        stock: {
+          price: {
+            total: totalPriceStock
+          }
+        },
+        borden: {
+          price: {
+            total: totalPriceBorden
+          }
+        }
       },
     };
   } catch (e) {
     return { props: { error: JSON.parse(JSON.stringify(e)) } };
+  }
+}
+
+async function calculateTotalPriceForDB(dbName) {
+  try {
+      const client = await clientPromise;
+      const db = client.db(dbName);
+      const collections = await db.listCollections().toArray();
+      
+      let totalPrice = 0;
+
+      for (const collection of collections) {
+          const collectionName = collection.name;
+          const coll = db.collection(collectionName);
+          
+          const documents = await coll.find({}).toArray();
+          
+          const collectionPrice = documents.reduce((sum, doc) => {
+              if (doc.price && !isNaN(doc.price)) {
+                  return sum + doc.price;
+              }
+              return sum;
+          }, 0);
+
+          totalPrice += collectionPrice;
+      }
+
+      return totalPrice;
+  } catch (error) {
+      console.error(`Error calculating total price for database ${dbName}:`, error);
+      return 1000000;
   }
 }
